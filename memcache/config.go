b3ea0f092e6d2eaeb097460a1474b5bf68ef4e9a
dbobrov <dbobrov@ozon.ru>
2021-04-20 16:51:10 +0300

    Added custom connection pool

---
+++ memcache/config.go
@@ -0,0 +1,25 @@
+package memcache
+
+import (
+	"time"
+)
+
+var DefaultConfig = Config{
+	InitConnections:    2,
+	MaxIdleConnections: 2,
+	ConnectTimeout:     100 * time.Millisecond,
+}
+
+type Config struct {
+	//Amount of connections which will be dialed during client initialization
+	InitConnections int
+	//Limit of idle connections, have to be more than amount of inflight requests at the moment
+	MaxIdleConnections int
+	//Limit of max open connections, if limit exceeded, try to create new connect will fail
+	MaxOpenConnections int
+
+	ConnectTimeout time.Duration
+
+	OpenConnectionHook  ConnectionHook
+	CloseConnectionHook ConnectionHook
+}
---
+++ memcache/conn.go
@@ -0,0 +1,72 @@
+package memcache
+
+import (
+	"bufio"
+	"net"
+	"time"
+)
+
+type ConnectionHook func(net.Addr, error)
+
+type Dialer interface {
+	Dial(addr net.Addr, timeout time.Duration) (Conn, error)
+}
+
+type dialer struct{}
+
+func (d dialer) Dial(addr net.Addr, timeout time.Duration) (Conn, error) {
+	nc, err := net.DialTimeout(addr.Network(), addr.String(), timeout)
+	if err == nil {
+		return &connV2{
+			nc:   nc,
+			addr: addr,
+			rw:   bufio.NewReadWriter(bufio.NewReader(nc), bufio.NewWriter(nc)),
+		}, nil
+	}
+
+	if ne, ok := err.(net.Error); ok && ne.Timeout() {
+		return nil, &ConnectTimeoutError{addr}
+	}
+
+	return nil, err
+}
+
+type Conn interface {
+	Addr() net.Addr
+	RW() *bufio.ReadWriter
+	SetTimeout(time.Duration)
+	SetPool(p *Pool)
+	Release()
+	Close() error
+}
+
+type connV2 struct {
+	nc   net.Conn
+	rw   *bufio.ReadWriter
+	addr net.Addr
+	p    *Pool
+}
+
+func (c connV2) Close() error {
+	return c.nc.Close()
+}
+
+func (c connV2) Addr() net.Addr {
+	return c.addr
+}
+
+func (c *connV2) RW() *bufio.ReadWriter {
+	return c.rw
+}
+
+func (c *connV2) SetTimeout(timeout time.Duration) {
+	c.nc.SetDeadline(time.Now().Add(timeout))
+}
+
+func (c *connV2) SetPool(p *Pool) {
+	c.p = p
+}
+
+func (c *connV2) Release() {
+	c.p.PutConn(c)
+}
